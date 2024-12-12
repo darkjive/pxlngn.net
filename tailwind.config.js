ddb5f347e97fb2d9a7dfc951c8a8f59e9bf1a365
darkjive <82544587+darkjive@users.noreply.github.com>
2024-12-12 12:05:35 +0100

    Remove animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,16 +20,6 @@ export default {
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
       },
 
-      animation: {
-        fade: 'fadeInUp 1s both',
-      },
-
-      keyframes: {
-        fadeInUp: {
-          '0%': { opacity: 0, transform: 'translateY(2rem)' },
-          '100%': { opacity: 1, transform: 'translateY(0)' },
-        },
-      },
     },
   },
   plugins: [
