d3da815beb1e40c04199803d8327024a7c828334
A <hi@pxlngn.net>
2025-11-19 12:59:30 +0100

    edit nav

--- src/navigation.ts
+++ src/navigation.ts
@@ -16,7 +16,7 @@ export const headerData = {
     },
     {
       text: 'Skills',
-      href: getPermalink('/#chart'),
+      href: getPermalink('/#skills'),
       icon: '',
       links: [],
     },
@@ -45,7 +45,7 @@ export const footerData = {
     },
     {
       title: 'Skills',
-      href: getPermalink('/#chart'),
+      href: getPermalink('/#skills'),
       icon: '',
       links: [],
     },
